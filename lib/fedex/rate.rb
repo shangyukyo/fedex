2e6f5473004663b219fe59f90f8efad85ae4ba86
shangyukyo <sdzc1014@163.com>
2023-08-30 15:09:07 +0800

    refine

--- lib/fedex/rate.rb
+++ lib/fedex/rate.rb
@@ -18,7 +18,7 @@ module Fedex
     #     @total_net_freight #The freight charge minus dicounts
     #     @total_surcharges #The total amount of all surcharges applied to this shipment
     #     @total_base_charge #The total base charge
-    attr_accessor :service_type, :transit_time, :rate_type, :rate_zone, :total_billing_weight, :total_freight_discounts, :total_net_charge, :total_taxes, :total_net_freight, :total_surcharges, :total_base_charge
+    attr_accessor :service_type, :transit_time, :rate_type, :rate_zone, :total_billing_weight, :total_freight_discounts, :total_net_charge, :total_taxes, :total_net_freight, :total_surcharges, :total_base_charge, :surcharge_list
     def initialize(options = {})
       @service_type = options[:service_type]
       @transit_time = options[:transit_time]
@@ -31,6 +31,7 @@ module Fedex
       @total_net_freight = options[:total_net_freight][:amount]
       @total_surcharges = options[:total_surcharges][:amount]
       @total_base_charge = options[:total_base_charge][:amount]
+      @surcharge_list = options[:surcharges]
       @total_net_fedex_charge = (options[:total_net_fe_dex_charge]||{})[:amount]
       @total_rebates = (options[:total_rebates]||{})[:amount]
     end
