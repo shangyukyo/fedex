f2ea6ca6d7851ca6b10887b50dff1fb6c7abd461
shangyukyo <sdzc1014@163.com>
2024-06-26 09:21:31 +0800

    refine

--- lib/fedex/request/rate.rb
+++ lib/fedex/request/rate.rb
@@ -8,7 +8,7 @@ module Fedex
 
         build_xml
         api_response = self.class.post(api_url, :body => build_xml)
-        puts api_response if @debug        
+        puts api_response.inspect if @debug        
         response = parse_response(api_response)
         if success?(response)
           rate_reply_details = response[:rate_reply][:rate_reply_details] || []
